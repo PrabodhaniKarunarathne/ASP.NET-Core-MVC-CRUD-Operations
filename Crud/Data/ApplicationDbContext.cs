06bdac8be947a0dcea938f5bc475f317baa6bd26
Prabodhani Karunarathne <karunar-ps19204@stu.kln.ac.lk>
2024-09-11 13:34:26 +0530

    First commit-1

---
+++ Crud/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Crud.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Crud.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options):base(options) 
+        {
+            
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
